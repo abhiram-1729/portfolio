d91fd3804ca79b3dc0887252972ceeca662c6c2c
abhiram-1729 <abhiramrangoon@gmail.com>
2026-04-04 16:46:59 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import { initCronJobs } from './utils/cronJobs.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
     initCronJobs();
 });
